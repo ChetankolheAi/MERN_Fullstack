fca286b22375cff5ed532a4a9b27573243290c03
Chetan Kolhe <Chetankolhe1020@gmail.com>
2025-01-17 23:30:04 +0530

    17/1/2025_JS

---
+++ index.java
@@ -0,0 +1,14 @@
+//encapsulation : - Proving control aceess to the Properties is called encapsultion 
+class dog{
+    void braks(){
+
+    }
+    void run(){
+
+    }
+
+}
+
+
+// s1 == s2 is use to check the refereneces
+// s1.equals(s2) is use to compare the content
